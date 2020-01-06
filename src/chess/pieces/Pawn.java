9aa991d4ba92e2a7c8a4e8ac91a83b790ac79fc2
cezuppo <czuppo@gmail.com>
2020-01-06 18:04:18 -0300

    Pawn.

---
+++ src/chess/pieces/Pawn.java
@@ -0,0 +1,65 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece{
+
+	public Pawn(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		if (getColor() == Color.WHITE) {
+			p.setValues(position.getRow() - 1, position.getColumn()); //One row above
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			p.setValues(position.getRow() - 2, position.getColumn()); //Two rows above
+			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			p.setValues(position.getRow() - 1, position.getColumn() - 1); //NW diagonal
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			p.setValues(position.getRow() - 1, position.getColumn() + 1); //NE diagonal
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+		}
+		else {
+			p.setValues(position.getRow() + 1, position.getColumn()); //One row below
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			p.setValues(position.getRow() + 2, position.getColumn()); //Two rows below
+			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			p.setValues(position.getRow() + 1, position.getColumn() - 1); //SW diagonal
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}			
+			p.setValues(position.getRow() + 1, position.getColumn() + 1); //SE diagonal
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}	
+		}
+		return mat;
+	}
+
+	@Override
+	public String toString() {
+		return "P";
+	}
+}
